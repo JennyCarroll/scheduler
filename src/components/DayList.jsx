ef999fea8eacffa9da6fa1dde0f60abde7fe52a7
Jenny Carroll <jennygc@telus.net>
2023-03-21 16:22:54 -0600

    creat DayList component, import in DayListItems, create array of DayListItems with keys and pass in props. Return parent container of array.

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -5,3 +5,20 @@
 
 // responsible for rendering a list of DayListItems components
 // it doesn't have any styles of its own so it doesn't need a DayList.scss file
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList (props) {
+  let array = props.days.map((day) => {
+    const selected = props.day === day.name
+    return (
+      <DayListItem 
+      key={day.id} 
+      name={day.name} 
+      spots={day.spots} 
+      selected={selected} 
+      setDay={props.setDay}/>
+    )
+  })
+  return (<ul>{array}</ul>)
+};
--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,11 +1,4 @@
-// name={day.name}
-// spots={day.spots} We use the spots prop for two purposes. To display the text "{props.spots} spots remaining" and to determine if the day is full. he DayListItem knows what it means to be full but not what it means to be selected. It uses this prop directly to determine which styles to apply.
-// selected={day.name === props.day}
-// setDay{props.setDay} 
-
-
-// if selected, should display:
-// name, interview spots remaining, 
+// If selected, this moldule is responsible for displaying the name, interview spots remaining, 
 // also the user must be able to select a particular day to view the interview information for that day.
 
 import React from "react";
@@ -14,7 +7,9 @@ import classNames from "classnames";
 
 
 export default function DayListItem(props) {
-let dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0})
+let dayClass = classNames("day-list__item", 
+{"day-list__item--selected": props.selected, 
+"day-list__item--full": props.spots === 0})
 
 function formatSpots () {
   if (props.spots === 0) {
