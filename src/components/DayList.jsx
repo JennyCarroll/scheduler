61b32419172bf155513705460f9bdbcd9ee8364d
Jenny Carroll <jennygc@telus.net>
2023-03-21 10:21:25 -0600

    create day list item as pass in props

---
+++ src/components/DayList.jsx
@@ -0,0 +1,7 @@
+// does not have a state. everything is passed downto it as props:
+// 1. days: Array of day objects {id name spots}
+// 2. day:string of the selected day
+// 3. setDay: function setDay(day) {}
+
+// responsible for rendering a list of DayListItems components
+// it doesn't have any styles of its own so it doesn't need a DayList.scss file
---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,21 @@
+// name={day.name}
+// spots={day.spots} We use the spots prop for two purposes. To display the text "{props.spots} spots remaining" and to determine if the day is full. he DayListItem knows what it means to be full but not what it means to be selected. It uses this prop directly to determine which styles to apply.
+// selected={day.name === props.day}
+// setDay{props.setDay} 
+
+
+// if selected, should display:
+// name, interview spots remaining, 
+// also the user must be able to select a particular day to view the interview information for that day.
+
+import React from "react";
+
+export default function DayListItem(props) {
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
+
