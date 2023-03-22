32fd90843875e4f41008c4adca995dd89af2d58b
Jenny Carroll <jennygc@telus.net>
2023-03-21 19:23:32 -0600

    build InterviewerList componenet, loop through props and build an array of InterviewListItems to return

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,8 +1,29 @@
-import React from 'React'
-import 'components/InterviewerList.scss'
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList (props) {
+// is being passed
+// interviewers: array
+// setInterviewer: function
+// interviewer: number
+export default function InterviewerList(props) {
+  let array = props.interviewers.map((interviewer) => {
+    const selected = props.interviewer === interviewer.id;
+    return (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={selected}
+        setInterviewer={props.setInterviewer}
+      ></InterviewerListItem>
+    );
+  });
+
   return (
-    <InterviewerListItem id={} name={} avatar={} selected ={}></InterviewerListItem>
-  )
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{array}</ul>
+    </section>
+  );
 }
--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import 'components/InterviewerListItem.scss'
+import React from "react";
+import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 // receives
-// id:number 
-// name: string 
-// avatar: url 
+// id:number
+// name: string
+// avatar: url
 // selected: boolean
 // setInterviewer (sets selected interviewer was on their id)
 // has two states, selected (highlight item and show name of interviewer
 // and not selected, show the image of the interviewer
-export default function InterviewerListItem (props) {
-  const conditionalClasses = classNames("interviewers__item-image", {"interviewers__item--selected": props.selected})
-  const interviewerName = props.selected ? props.name : ""
+export default function InterviewerListItem(props) {
+  const conditionalClasses = classNames("interviewers__item-image", {
+    "interviewers__item--selected": props.selected,
+  });
+  const interviewerName = props.selected ? props.name : "";
   return (
-    <li className={conditionalClasses} onClick={() => props.setInterviewer(props.id)}>
-  <img
-    className="interviewers__item-image"
-    src={props.avatar}
-    alt={props.name}
-  />
-  {interviewerName}
-</li>
-  )
+    <li
+      className={conditionalClasses}
+      onClick={() => props.setInterviewer(props.id)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {interviewerName}
+    </li>
+  );
 }
