31a9b791c840c563b57aad24b1a855bf17b49abd
Jenny Carroll <jennygc@telus.net>
2023-03-22 09:57:47 -0600

    create show component, require it in index.js and application.js and pass in props. create stories

---
+++ src/components/Appointment/Show.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+// When the <Show> view is visible a user can hover over the appointment
+//  revealing an edit and delete button. They can click either of these
+//  buttons to edit or delete the appointment.
+
+// The <Show> component should accept the following props:
+
+// student:String eg. "Lydia Miller-Jones"
+// interviewer:Object we can use the interview object that already exists in stories/index.js for this
+// onEdit:Function to be called when the user clicks the Edit button
+// onDelete:Function to be called when the user clicks the Delete button
+
+export default function Show(props) {
+  return (
+    <main className="appointment__card appointment__card--show">
+      <section className="appointment__card-left">
+        <h2 className="text--regular">{props.student}</h2>
+        <section className="interviewer">
+          <h4 className="text--light">Interviewer</h4>
+          <h3 className="text--regular">{props.interviewer.name}</h3>
+        </section>
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <img
+            onClick={props.onEdit}
+            className="appointment__actions-button"
+            src="images/edit.png"
+            alt="Edit"
+          />
+          <img
+            onClick={props.onDelete}
+            className="appointment__actions-button"
+            src="images/trash.png"
+            alt="Delete"
+          />
+        </section>
+      </section>
+    </main>
+  );
+}
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 // A user will be able to create, edit and delete an appointment
 // The <Appointment> component will display differently depending
@@ -11,11 +13,18 @@ import Header from "./Header";
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header />
       {/* header displays the time for the appointment with a separater */}
+      <Header time={props.time} />
       {/* every other component is shown conditionally */}
       {/* !appointment ? empty (click on add button (image) will call on add action) */}
+      <Empty onAdd={props.onAdd} />
       {/* appointment show (displays name of student and interiewer scheduled, clicking on edit will call onEdit action, clicking on the trash icon will call the on delete action) */}
+      <Show
+        student={props.student}
+        interviewer={props.interviewer}
+        onEdit={props.onEdit}
+        onDelete={props.onDelete}
+      />
       {/* before deleting an appointment confirm (cancel button triggers onCancel action, confirm button triggers onConfirm action) */}
       {/* async operations ? status (saving or deleting) */}
       {/* error (saving  or deleting, when close button is clicked onClose action is called) */}
